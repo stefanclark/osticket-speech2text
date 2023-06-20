6b911ba26b4066f2e5289a606ddd383293913684
Stefan Clark <stefanclark@gmail.com>
2023-06-20 21:21:47 +0100

    Refactor code + replace file_get_contents with cURL

--- speech2text.php
+++ speech2text.php
@@ -31,52 +31,66 @@ class Speech2TextPlugin extends Plugin
 
     public function processAttachments($ticket)
     {
+        // From class.file.php:
+        //
+        // function getData()
+        //     # XXX: This is horrible, and is subject to php's memory
+        //     #      restrictions, etc. Don't use this function!
+        //
+        // This plugin uses the above function as it works across Attachent Storage configurations
+        //   including 'in the database' and 'filesystem'
+
         $config = $this->getConfig(self::$pluginInstance);
 
-        $title = $config->get('speech2text-title') ?: 'Speech to Text';
-        $poster = $config->get('speech2text-poster') ?: 'SYSTEM';
-        $note = '';
+        if (!$apiKey = $config->get('speech2text-apikey')) {
+            $this->log(__FUNCTION__ . ': API Key not configured', LOG_WARN);
+            return;
+        }
+
         $message = $ticket->getLastMessage();
-        if ($message && $message->getNumAttachments() > 0) {
-            $attachments = array();
-            foreach ($message->getAttachments() as $attachment) {
-                if (in_array($fileType = $attachment->getFile()->getType(), ['audio/wav'])) {
-                    $title .= ': ' . $attachment->getName();
-                    $location = $config->get('speech2text-location') ?: 'uksouth';
-                    $msSpeechUrl = "https://{$location}.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-GB";
-                    $apiKey = $config->get('speech2text-apikey');
-                    if (!empty($apiKey)) {
-                        // From class.file.php:
-                        // function getData() {
-                        //     # XXX: This is horrible, and is subject to php's memory
-                        //     #      restrictions, etc. Don't use this function!
-                        $att = $attachment->getFile()->getData();
-                        $opts = array('http' =>
-                            array(
-                                'method' => 'POST',
-                                'header' => array(
-                                    "Ocp-Apim-Subscription-Key: $apiKey",
-                                    "Content-Type: $fileType"
-                                ),
-                                'content' => $att
-                            )
-                        );
-                        $context = stream_context_create($opts);
-                        $note = file_get_contents($msSpeechUrl, false, $context);
-                        if (!empty($note)) {
-                            $this->log(__FUNCTION__ . ': Success, Internal Note should be added');
-                        } else {
-                            $note = "Transcription unsuccessful. ({$http_response_header[0]})";
-                            $this->log(__FUNCTION__ . ': Transcription unsuccessful. ' . print_r($http_response_header, true));
-                        }
-                        $ticket->logNote($title, $note, $poster, false);
-                    } else {
-                        $this->log(__FUNCTION__ . ': API Key not configured', LOG_WARN);
-                    }
+        if (!$message || $message->getNumAttachments() == 0) {
+            $this->log(__FUNCTION__ . ': No attachments');
+            return;
+        }
+
+        $poster = $config->get('speech2text-poster') ?: 'SYSTEM';
+        $title = $config->get('speech2text-title') ?: 'Speech to Text';
+        $location = $config->get('speech2text-location') ?: 'uksouth';
+        $msSpeechUrl = "https://{$location}.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-GB";
+
+        foreach ($message->getAttachments() as $attachment) {
+            if (!in_array($fileType = $attachment->getFile()->getType(), ['audio/wav', 'audio/x-wav'])) {
+                $this->log(__FUNCTION__ . ': File type not valid. ' . $fileType);
+                continue;
+            }
+            $note = '';
+            $note_title = $title . ': ' . $attachment->getName();
+
+            $header = array(
+                    "Ocp-Apim-Subscription-Key: $apiKey",
+                    "Content-Type: $fileType"
+                    );
+            $payload = $attachment->getFile()->getData();
+            $curl = curl_init($msSpeechUrl);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+            $response = curl_exec($curl);
+            if (empty($response)) {
+                $note = 'Transcription unsuccessful. (' . curl_errno($curl) . ')';
+                $this->log(__FUNCTION__ . ': Transcription unsuccessful. ' . curl_error($curl));
+            } else {
+                $result = json_decode($response);
+                if ($result->RecognitionStatus == 'Success') {
+                    $note = $result->DisplayText ?: '[NO SPEECH RECOGNISED]';
+                    $this->log(__FUNCTION__ . ': Success! Internal Note should be added');
+                } else {
+                    $note = "Transcription unsuccessful. ({$result->RecognitionStatus})";
+                    $this->log(__FUNCTION__ . ': Transcription unsuccessful. ' . print_r($result, true));
                 }
             }
-        } else {
-            $this->log(__FUNCTION__ . ': No attachments');
+            $ticket->logNote($note_title, $note, $poster, false);
         }
     }
 
